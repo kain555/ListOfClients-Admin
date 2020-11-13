2f044be79f3abd3ab396a10d7282764b35aa2657
kain555 <56169403+kain555@users.noreply.github.com>
2020-11-13 22:11:15 +0100

    Add components and services
    Add new:
    - Components: Client Form and Client List
    - Services: Clients Service

--- ListOfClients/src/app/app.module.ts
+++ ListOfClients/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientFormComponent } from './components/client-form/client-form.component';
+import { ClientsListComponent } from './components/clients-list/clients-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent,
+    ClientsListComponent
   ],
   imports: [
     BrowserModule,
