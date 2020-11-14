e43881831e2fa0110abd9d71f62150a140a72ecd
kain555 <56169403+kain555@users.noreply.github.com>
2020-11-14 13:07:01 +0100

    Add new component and table.
    Add header component.
    Add table with json records and features:
    - Paginator
    - Sorting
    - Filtering

--- ListOfClients/src/app/app.module.ts
+++ ListOfClients/src/app/app.module.ts
@@ -7,16 +7,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClientFormComponent } from './components/client-form/client-form.component';
 import { ClientsListComponent } from './components/clients-list/clients-list.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HeaderComponent } from './components/header/header.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+import {MatDialogModule} from '@angular/material/dialog';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     ClientFormComponent,
-    ClientsListComponent
+    ClientsListComponent,
+    HeaderComponent
   ],
+  exports: [MatFormFieldModule, MatInputModule],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSortModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ListOfClients/src/app/components/clients-list/clients-list.component.ts
+++ ListOfClients/src/app/components/clients-list/clients-list.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+import clientsData from '../../data/clients.json';
+import { ClientFormComponent } from '../client-form/client-form.component';
+
+export interface Client {
+  id: number;
+  name: string;
+  surname: string;
+  dataOfBirth: string;
+  industry:string;
+  subcategory: string;
+  telephone: number;
+  email: string;
+}
 
 @Component({
   selector: 'app-clients-list',
   templateUrl: './clients-list.component.html',
-  styleUrls: ['./clients-list.component.css']
+  styleUrls: ['./clients-list.component.css'],
+  providers: [
+    { provide: MatFormFieldControl, useExisting: ClientsListComponent }   
+  ]
 })
 export class ClientsListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'surname', 'dataOfBirth', 'industry', 'subcategory', 'telephone', 'email', 'edit'];
+  dataSource: MatTableDataSource<Client>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(public dialog: MatDialog) { 
+    this.dataSource = new MatTableDataSource(clientsData);
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  addNewClient(id:number){
+    const dialogRef = this.dialog.open(ClientFormComponent, {
+      width: '50%',
+      height: '80%'
+    }); 
+    setTimeout(() => {
+      dialogRef.close();
+    }, 15000);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
